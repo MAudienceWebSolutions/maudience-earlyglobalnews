bc6331ebe5a32c9f56c9bcbd1f0bd2e264801ca6
Pete Lower <pete.lower@gmail.com>
2015-05-06 17:54:29 -0700

    add front page template

---
+++ wordpress/wp-content/themes/maudience-earlyglobalnews/front-page.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
+				<?php comments_template( '', true ); ?>
+			<?php endwhile; // end of the loop. ?>
+
+			<h2>Recent Posts</h2>
+			<ul>
+			<?php
+				$recent_posts = wp_get_recent_posts();
+				foreach( $recent_posts as $recent ){
+					echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
+				}
+			?>
+			</ul>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
